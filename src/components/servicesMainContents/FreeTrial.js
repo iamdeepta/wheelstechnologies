a2e0a0098a08fbaeb6dbb76b1ece21425fdc00db
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2021-09-29 19:40:27 +0600

    updated

--- src/components/servicesMainContents/FreeTrial.js
+++ src/components/servicesMainContents/FreeTrial.js
@@ -1,14 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import AppUrl from "../../classes/AppUrl";
+import "./css/free_trial.css";
 
 const FreeTrial = () => {
   const [data, setData] = useState([]);
   const [data1, setData1] = useState([]);
+  const [input, setInput] = useState("");
+  const [seo_input_error, setSeoInputError] = useState(false);
+
+  const seo_start_loading = useRef(null);
 
   useEffect(() => {
     getData();
   }, []);
+
+  // useEffect(() => {
+  //   getSeoData();
+  // }, []);
+
+  // function getSeoData() {
+  //   fetch(
+  //     "https://seo-analysis.p.rapidapi.com/seocheck_multiple3.1.php?api-call=1&external=1&keyword=covidsurokkha&url=https%3A%2F%2Fcovidsurokkha.com",
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         "x-rapidapi-host": "seo-analysis.p.rapidapi.com",
+  //         "x-rapidapi-key":
+  //           "f810671fb5msh3c219e1dc3a3725p1965c7jsn84d272eb4e57",
+  //       },
+  //     }
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data.data);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // }
 
   function getData() {
     axios
@@ -23,6 +53,15 @@ const FreeTrial = () => {
       .catch(function (error) {
         console.log(error);
       });
+  }
+
+  function check_now() {
+    if (input === "") {
+      setSeoInputError(true);
+    } else {
+      seo_start_loading.current.style.display = "block";
+      setSeoInputError(false);
+    }
   }
 
   return (
@@ -47,19 +86,33 @@ const FreeTrial = () => {
                     {data.servicesection2_title}
                   </h2>
                   <p>{data.servicesection2_description}</p>
+                  {seo_input_error === true ? (
+                    <>
+                      <div className="seo_input_error">
+                        Please enter url first
+                      </div>
+                    </>
+                  ) : (
+                    ""
+                  )}
                 </div>
                 <div
                   className="cta__form mb-25 wow fadeInUp"
                   data-wow-delay=".5s"
                 >
-                  <form action="#">
-                    <input
-                      type="email"
-                      placeholder="Enter Your Website's URL"
-                    />
-                    <button type="submit">Check Now</button>
-                  </form>
+                  {/* <form action="#"> */}
+                  <input
+                    type="email"
+                    placeholder="Enter Your Website's URL"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                  />
+                  <button type="submit" onClick={() => check_now()}>
+                    Check Now
+                  </button>
+                  {/* </form> */}
                 </div>
+
                 <div
                   className="cta__features wow fadeInUp"
                   data-wow-delay=".7s"
@@ -73,6 +126,19 @@ const FreeTrial = () => {
                     <li>Connect Customer</li> */}
                   </ul>
                 </div>
+                {input !== "" ? (
+                  <>
+                    <div className="seo_main_div">
+                      <h4 className="seo_url_text">URL: {input}</h4>
+                      <div
+                        className="seo_start_loading"
+                        ref={seo_start_loading}
+                      ></div>
+                    </div>
+                  </>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
           </div>
