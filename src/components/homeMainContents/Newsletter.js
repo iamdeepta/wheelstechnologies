5c7ed4f720c8400e2d3dff2e07191e1f8da3972a
iamdeepta <deepta.barua@northsouth.edu>
2021-12-01 20:43:54 +0600

    newsletter section

--- src/components/homeMainContents/Newsletter.js
+++ src/components/homeMainContents/Newsletter.js
@@ -1,7 +1,135 @@
-import React from "react";
-import "./css/newsletter.scss";
+import React, { useState } from "react";
+import "./css/newsletter.css";
+import { gsap } from "gsap";
+import NewsletterSvg from "./NewsletterSvg";
 
 const Newsletter = () => {
+  const [newsEmail, setNewsEmail] = useState("");
+  //gsap code for paper plane button
+  document.querySelectorAll(".newsletter_btn").forEach((button) => {
+    console.log("ok");
+    let getVar = (variable) =>
+      getComputedStyle(button).getPropertyValue(variable);
+
+    button.addEventListener("click", (e) => {
+      if (!button.classList.contains("active")) {
+        button.classList.add("active");
+
+        gsap.to(button, {
+          keyframes: [
+            {
+              "--left-wing-first-x": 50,
+              "--left-wing-first-y": 100,
+              "--right-wing-second-x": 50,
+              "--right-wing-second-y": 100,
+              duration: 0.2,
+              onComplete() {
+                gsap.set(button, {
+                  "--left-wing-first-y": 0,
+                  "--left-wing-second-x": 40,
+                  "--left-wing-second-y": 100,
+                  "--left-wing-third-x": 0,
+                  "--left-wing-third-y": 100,
+                  "--left-body-third-x": 40,
+                  "--right-wing-first-x": 50,
+                  "--right-wing-first-y": 0,
+                  "--right-wing-second-x": 60,
+                  "--right-wing-second-y": 100,
+                  "--right-wing-third-x": 100,
+                  "--right-wing-third-y": 100,
+                  "--right-body-third-x": 60,
+                });
+              },
+            },
+            {
+              "--left-wing-third-x": 20,
+              "--left-wing-third-y": 90,
+              "--left-wing-second-y": 90,
+              "--left-body-third-y": 90,
+              "--right-wing-third-x": 80,
+              "--right-wing-third-y": 90,
+              "--right-body-third-y": 90,
+              "--right-wing-second-y": 90,
+              duration: 0.2,
+            },
+            {
+              "--rotate": 50,
+              "--left-wing-third-y": 95,
+              "--left-wing-third-x": 27,
+              "--right-body-third-x": 45,
+              "--right-wing-second-x": 45,
+              "--right-wing-third-x": 60,
+              "--right-wing-third-y": 83,
+              duration: 0.25,
+            },
+            {
+              "--rotate": 55,
+              "--plane-x": -8,
+              "--plane-y": 24,
+              duration: 0.2,
+            },
+            {
+              "--rotate": 40,
+              "--plane-x": 45,
+              "--plane-y": -180,
+              "--plane-opacity": 0,
+              duration: 0.3,
+              onComplete() {
+                setTimeout(() => {
+                  button.removeAttribute("style");
+                  gsap.fromTo(
+                    button,
+                    {
+                      opacity: 0,
+                      y: -8,
+                    },
+                    {
+                      opacity: 1,
+                      y: 0,
+                      clearProps: true,
+                      duration: 0.3,
+                      onComplete() {
+                        button.classList.remove("active");
+                      },
+                    }
+                  );
+                }, 2000);
+              },
+            },
+          ],
+        });
+
+        gsap.to(button, {
+          keyframes: [
+            {
+              "--text-opacity": 0,
+              "--border-radius": 0,
+              "--left-wing-background": getVar("--primary-darkest"),
+              "--right-wing-background": getVar("--primary-darkest"),
+              duration: 0.1,
+            },
+            {
+              "--left-wing-background": getVar("--primary"),
+              "--right-wing-background": getVar("--primary"),
+              duration: 0.1,
+            },
+            {
+              "--left-body-background": getVar("--primary-dark"),
+              "--right-body-background": getVar("--primary-darkest"),
+              duration: 0.4,
+            },
+            {
+              "--success-opacity": 1,
+              "--success-scale": 1,
+              duration: 0.25,
+              delay: 0.25,
+            },
+          ],
+        });
+      }
+    });
+  });
+
   return (
     <>
       <section className="about__area grey-bg-3 pt-40 pb-120 p-relative">
@@ -24,6 +152,7 @@ const Newsletter = () => {
                 className="about__thumb-3 wow fadeInLeft"
                 data-wow-delay=".3s"
               >
+                <NewsletterSvg />
                 {/* <img
                   src={AppUrl.image_url + data.homesection3_image}
                   alt="features images"
@@ -52,14 +181,35 @@ const Newsletter = () => {
                 <div className="newsletter_input_div">
                   <input
                     type="text"
+                    placeholder="Email address"
                     className="form-control newsletter_input"
+                    onChange={(e) => setNewsEmail(e.target.value)}
                   />
-                  <button class="newsletter_btn">
-                    <span class="default">Send</span>
-                    <span class="success">Sent</span>
-                    <div class="left"></div>
-                    <div class="right"></div>
-                  </button>
+                  {newsEmail.indexOf(".") > -1 &&
+                  newsEmail.indexOf("@") > -1 ? (
+                    <>
+                      <button class="newsletter_btn">
+                        <span class="default">Send</span>
+                        <span class="success">
+                          <i className="fa fa-check"></i> Sent
+                        </span>
+                        <div class="left"></div>
+                        <div class="right"></div>
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        class="newsletter_btn newsletter_btn_fade"
+                        disabled={true}
+                      >
+                        <span class="default">Send</span>
+                        <span class="success">Sent</span>
+                        <div class="left"></div>
+                        <div class="right"></div>
+                      </button>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
